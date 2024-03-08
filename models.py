577633e30a7701f47cdf7d0ae57d77120a240593
nrgsghanei2001 <nargesghanei031@gmail.com>
2024-03-08 21:14:18 +0330

    add refracroy period with spike firing dynamic to models

--- models.py
+++ models.py
@@ -12,7 +12,9 @@ class LIF(Behavior):
         self.u_reset = self.parameter("u_reset")
         self.threshold = self.parameter("threshold")
         self.R = self.parameter("R")
-        self.tau_r = self.parameter("tau_r", 10)
+        self.tau_r = self.parameter("tau_r", 15)
+        self.tau_decay = self.parameter("tau_decay", 2)
+        self.theta_reset = self.parameter("theta_reset", 30)
             
         ng.num_spikes = ng.vector(mode=0)    # number of spikes 
         ng.v = ng.vector(mode=self.u_rest)  # initialize v with u-rest
@@ -28,17 +30,17 @@ class LIF(Behavior):
 		# refractory period
         if ng.refractory_time > 0:
             ng.refractory_time -= 1
-            ng.v += (leakage  / self.tau) * ng.network.dt  
+            ng.v += (leakage  / self.tau_decay) * ng.network.dt  
                
 		# firing
         else:
-            if ng.v >= self.threshold:
+            if ng.v >= self.theta_reset:
                 ng.num_spikes += 1
                 ng.refractory_time = self.tau_r
 			
             # reset
-            ng.spike = ng.v >= self.threshold
-            ng.v[ng.spike] = self.u_reset
+            # ng.spike = ng.v >= self.threshold
+            # ng.v[ng.spike] = self.u_reset
             ng.v += ((leakage + currents) / self.tau) * ng.network.dt 
         
 
@@ -57,7 +59,9 @@ class ELIF(Behavior):
         self.threshold = self.parameter("threshold")
         self.delta_t = self.parameter("delta_t")
         self.R = self.parameter("R")
-        self.tau_r = self.parameter("tau_r", 10)
+        self.tau_r = self.parameter("tau_r", 15)
+        self.tau_decay = self.parameter("tau_decay", 2)
+        self.theta_reset = self.parameter("theta_reset", 30)
 
         ng.v = ng.vector(mode=self.u_rest)	
         ng.spikes = ng.vector(mode=0)
@@ -76,19 +80,21 @@ class ELIF(Behavior):
         # refractory period
         if ng.refractory_time > 0:
             ng.refractory_time -= 1
-            membrane_potential_change = linear + F
-            ng.v += (membrane_potential_change / self.tau) * ng.network.dt
+            ng.v += (linear / (self.tau_decay)) * ng.network.dt
+
         else:
             # firing
-            if ng.v >= self.threshold:
+            if ng.v >= self.theta_reset:
                 ng.num_spikes += 1
                 ng.refractory_time = self.tau_r
                         
-            ng.spike = ng.v >= self.threshold
+            ng.spike = ng.v >= self.theta_reset
             # reset
-            ng.v[ng.spike] = self.u_reset
-            membrane_potential_change = linear + F + ri
-            ng.v += (membrane_potential_change / self.tau) * ng.network.dt
+            if ng.v < self.theta_reset:
+                membrane_potential_change = linear + F + ri
+                ng.v += (membrane_potential_change / self.tau) * ng.network.dt
+                if ng.v > self.theta_reset:
+                    ng.v = ng.vector(mode=self.theta_reset)
 
 
 # Adaptive Exponential LIF
@@ -104,7 +110,9 @@ class AELIF(Behavior):
         self.theta_rh = self.parameter("theta_rh")
         self.alpha = self.parameter("alpha")
         self.beta = self.parameter("beta")
-        self.tau_r = self.parameter("tau_r", 10)
+        self.tau_r = self.parameter("tau_r", 15)
+        self.tau_decay = self.parameter("tau_decay", 2)
+        self.theta_reset = self.parameter("theta_reset", 30)
 
         ng.v = ng.vector(mode=self.u_rest)
         ng.spikes = ng.vector(mode=0)
@@ -120,21 +128,18 @@ class AELIF(Behavior):
             ng.refractory_time -= 1
             linear = -(ng.v - self.u_rest)
             v1 = (ng.v - self.theta_rh) / self.delta_t
-            F = self.delta_t * np.exp(v1)
-            u = linear + F 
+            u = linear 
             dvdt = u - self.R * ng.w
-            ng.v += dvdt / self.tau_m
+            ng.v += dvdt / self.tau_decay
 
         else:
             # firing
-            if ng.v > self.threshold:
+            if ng.v >= self.theta_reset:
                 ng.num_spikes += 1
                 ng.refractory_time = self.tau_r
 
-            ng.spike = ng.v >= self.threshold
+            ng.spike = ng.v >= self.theta_reset
 
-            # reset
-            ng.v[ng.spike] = self.u_reset
 
             # dynamic
             linear = -(ng.v - self.u_rest)
@@ -143,7 +148,11 @@ class AELIF(Behavior):
             F = self.delta_t * np.exp(v1)
             u = linear + F + ri
             dvdt = u - self.R * ng.w
-            ng.v += dvdt / self.tau_m
+            if ng.v < self.theta_reset:
+                ng.v += dvdt / self.tau_m
+                if ng.v > self.theta_reset:
+                    ng.v = ng.vector(mode=self.theta_reset)
+
             
         # adaptation
         adaptation = self.beta * self.tau_w * ng.num_spikes 
