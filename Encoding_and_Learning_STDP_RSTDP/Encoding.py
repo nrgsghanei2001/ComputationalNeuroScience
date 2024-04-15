6c4bfa9ac59623bb86a6b31eabe27d40f0a84383
nrgsghanei2001 <nargesghanei031@gmail.com>
2024-04-15 21:58:51 +0330

    completed time-to-first-spike model of encoding

--- Encoding_and_Learning_STDP_RSTDP/Encoding.py
+++ Encoding_and_Learning_STDP_RSTDP/Encoding.py
@@ -12,23 +12,27 @@ class TimeToFirstSpikeEncoding:
         self.data = data
 
     def scale_data(self):
-        self.data = abs(self.data - self.range1 + self.range0)
-        # Scaling data to values to time range
-        self.data = (self.data - self.range0) * (self.time - 1) // (self.range1 - self.range0)
+        _data = self.data.flatten()
+        times = self.time - (_data * (self.time / _data.max())).long()
+        return times
+        # self.data = abs(self.data - self.range1 + self.range0)
+        # # Scaling data to values to time range
+        # self.data = (self.data - self.range0) * (self.time - 1) // (self.range1 - self.range0)
 
     def encode(self):
-        self.scale_data()
-        print(self.data.shape)
-        spikes = torch.Tensor(self.time, self.num_neurons)
-        for i in range(self.time):
-            spikes[i] = self.data == i
-        
-        return spikes
-        # _data = self.data.flatten()
+        times = self.scale_data()
         # spikes = torch.zeros((self.time + 1, self.num_neurons))
-        # times = self.time - (_data * (self.time / _data.max())).long()
-        # print(times.shape)
         # spikes[times, torch.arange(self.num_neurons)] = 1
-
         # spikes = spikes[:-1]
         # return spikes
+    
+        spikes = torch.zeros((self.time, self.num_neurons))
+        for j in range(self.num_neurons):
+            for i in range(self.time):
+                if i == times[j]:
+                    spikes[i][j] = 1
+
+        
+        return spikes
+        
+        
