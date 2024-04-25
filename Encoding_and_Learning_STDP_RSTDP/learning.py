21359095700a700db646f551c62d3f193746831a
nrgsghanei2001 <nargesghanei031@gmail.com>
2024-04-25 15:50:27 +0330

    Add R-STDP learning rule

--- Encoding_and_Learning_STDP_RSTDP/learning.py
+++ Encoding_and_Learning_STDP_RSTDP/learning.py
@@ -19,7 +19,7 @@ class STDP(Behavior):
         A1 = self.lr[0] * src_s * dst_t
         A2 = self.lr[1] * dst_s * src_t
         sg.W += -A1 + A2
-        print(sg.W)
+        # print(sg.W)
 
     def mask_spike_trace(self, inp, mask):
         if len(inp.size()) > 1:
@@ -31,71 +31,72 @@ class STDP(Behavior):
 
 
 
+class RSTDP(Behavior):
 
-# class STDP2(LearningRule):
-#     """
-#     Spike-Time Dependent Plasticity learning rule.
-#     Implement the dynamics of STDP learning rule.You might need to implement\
-#     different update rules based on type of connection.
-#     """
+    def initialize(self, sg):
+        self.lr = self.parameter("lr", [3, 5])
+        self.weight_decay = self.parameter("wd", 2)
+        self.tau_c = self.parameter("tau_c", 500)
+        self.wmin = self.parameter("wmin", 10)
+        self.wmax = self.parameter("wmax", 30)
+        self.pct = self.parameter("pct", 40)
+        self.d = self.parameter("d", 1)
+        self.tau_d = self.parameter("tau_d", 10)
+        sg.C = torch.zeros(*sg.W.size())
 
-#     def __init__(
-#         self,
-#         connection: AbstractConnection,
-#         lr: Optional[Union[float, Sequence[float]]] = None,
-#         weight_decay: float = 0.,
-#         **kwargs
-#     ) -> None:
-#         super().__init__(
-#             connection=connection,
-#             lr=lr,
-#             weight_decay=weight_decay,
-#             **kwargs
-#         )
-#         """
-#         TODO.
-#         Consider the additional required parameters and fill the body\
-#         accordingly.
-#         """
-#         self.connection = connection
-#         self.lr = lr
-#         self.weight_decay
-        
-#         self.dt = kwargs.get("dt", 1.)
 
-#     def update(self, **kwargs) -> None:
-#         """
-#         TODO.
-#         Implement the dynamics and updating rule. You might need to call the\
-#         parent method.
-#         """
-#         mask = torch.ones(*self.connection.w.size())
-        
-#         pre_s = self.connection.pre.s
 
-#         if len(pre_s.size()) > 1:
-#             pre_s = torch.t(pre_s)
-#         else:
-#             pre_s = pre_s.view(pre_s.size(0), -1)
-#         pre_s = pre_s * mask
-        
-#         post_traces = self.connection.post.traces * mask
-#         # print(post_traces)
-#         p1 = self.lr[0] * pre_s * post_traces
-        
-        
-#         post_s = self.connection.post.s * mask
-        
-#         pre_traces = self.connection.pre.traces
-#         if len(pre_traces.size()) > 1:
-#             pre_traces = torch.t(pre_traces)
-#         else:
-#             pre_traces = pre_traces.view(pre_traces.size(0), -1)
-#         pre_traces = pre_traces * mask
-#         # print(pre_traces)
-#         p2 = self.lr[1] * post_s * pre_traces
-#         # print(self.lr[1])
-#         self.connection.w += ((-p1) + p2) * self.dt
-#         self.connection.w = torch.clamp(self.connection.w, self.connection.wmin, self.connection.wmax)
+    def forward(self, sg):
 
-#         super().update()
+        mask = torch.ones(*sg.W.size())
+        src_s = self.mask_spike_trace(sg.src.spike, mask)
+        dst_s = sg.dst.spike * mask
+        src_t = self.mask_spike_trace(sg.src.trace, mask)
+        dst_t = sg.dst.trace * mask
+        
+        A1 = self.lr[0] * src_s * dst_t
+        A2 = self.lr[1] * dst_s * src_t
+        stdp = -A1 + A2
+
+        da = self.reward(sg, sg.dst.spike)
+        # dd_dt = -(self.d/self.tau_d) + da
+        self.d = da
+        
+        dc_dt = -sg.C/self.tau_c + stdp * ((src_s + dst_s) > 0)
+        sg.C += dc_dt 
+
+        sg.W += sg.C * self.d
+        for i in range(sg.W.shape[0]):
+            for j in range(sg.W.shape[1]):
+                if sg.W[i][j] > self.wmax:
+                    sg.W[i][j] = self.wmax
+                if sg.W[i][j] < self.wmin:
+                    sg.W[i][j] = self.wmin
+
+        print(sg.W)
+        print(src_s)
+        print(da)
+        print()
+
+
+    def mask_spike_trace(self, inp, mask):
+        if len(inp.size()) > 1:
+            inp = torch.t(inp)
+        else:
+            inp = inp.view(inp.size(0), -1)
+
+        return inp * mask
+    
+    def reward(self, sg, output):
+        input_pattern = sg.src.pn 
+        print(input_pattern, output)
+        if output[0] == False and output[1] == False:
+            return 0
+        elif output[0] == True and output[1] == True:
+            return -2
+        elif input_pattern == 0 and output[0] == True:
+            return 2
+        elif input_pattern == 1 and output[1] == True:
+            return 2
+        else:
+            return -2
--- Encoding_and_Learning_STDP_RSTDP/lif.py
+++ Encoding_and_Learning_STDP_RSTDP/lif.py
@@ -14,6 +14,7 @@ class LIF(Behavior):
 		self.R = self.parameter("R")
 		ng.N = self.parameter("N", 10)
 		ng.v = ng.vector(mode=self.u_rest)  # initialize v with u-rest
+		ng.v[0] = -45
 		ng.spikes = ng.vector(mode=0)       # save spike times
 		ng.trace = ng.vector(mode=0)      
 		# firing
@@ -25,7 +26,7 @@ class LIF(Behavior):
 	def forward(self, ng):
 		# firing
 		ng.spike = ng.v >= self.threshold
-		ng.spike[-1] = False
+		# ng.spike[-1] = False
 		ng.num_spikes = torch.sum(ng.spike).item()
 		ng.population_activity = ng.num_spikes / ng.N
 		ng.trace += -ng.trace/self.tau_trace
@@ -42,6 +43,7 @@ class InputPattern(Behavior):
 		# set models parameters
 		ng.pattern = self.parameter("pattern")
 		ng.pattern2 = self.parameter("pattern2", None)
+		ng.pn = 0
 		self.ch_pattern_time = self.parameter("cpt", 50)
 		self.sleep = self.parameter("sleep", 10)
 		self.sleep_past = 0
@@ -56,10 +58,11 @@ class InputPattern(Behavior):
 		if ng.iter >= self.ch_pattern_time and self.sleep_past < self.sleep:
 			self.sleep_past += 1
 			ng.pattern = ng.pattern2
+			ng.pn = 1
 		else:
 			ng.spike = ng.pattern[ng.iter] == 1
-			ng.spike[-1] = False
-			print(ng.spike)
+			# ng.spike[-1] = False
+			# print(ng.spike)
 			ng.iter += 1
 			ng.trace += -ng.trace/self.tau_trace
 			ng.trace[ng.spike] += 1
